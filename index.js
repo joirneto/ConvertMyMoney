9a173ffe372caf622397f242df7dc1659878923f
joirneto <joirneto@gmail.com>
2021-11-04 09:29:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res)=>{
   res.send(`Server listenig at PORT:${PORT}`);
